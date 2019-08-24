1bf807bd432252ea657ee0418a0403b346501265
James Carlson <jxxcarlson@mac.com>
2019-08-24 09:05:52 +0200

    Remove all HTree stuff

--- unused/Element.elm
+++
@@ -1,9 +0,0 @@
-module Element exposing (Element(..), level, string)
-
-type Element = Element Int String
-
-level : Element -> Int
-level (Element k _ ) = k
-
-string : Element -> String
-string (Element _ str) = str
